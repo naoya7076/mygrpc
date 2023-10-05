e4722ea0862021610ad374a4fdc7f29fb61794d4
naoya7076 <naoya7076@gmail.com>
2023-10-05 23:42:07 +0900

    update myUnaryServerInterceptor1

--- cmd/server/unaryInterceptor.go
+++ cmd/server/unaryInterceptor.go
@@ -1,6 +1,15 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
 func myUnaryServerInterceptor1(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	log.Println("[pre] Unary interceptor 1: ", info.FullMethod)
 	res, err := handler(ctx, req)
-	log.Println("[post] Unary interceptor 1: ", m)
+	log.Println("[post] Unary interceptor 1: ")
 	return res, err
 }
